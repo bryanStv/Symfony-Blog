ce35022dc45895914925cfab327dc0b6a73a5a3e
bryanStv <bryans.oyola@gmail.com>
2024-10-10 10:34:18 +0200

    Controller contact y sigle_post(falta arreglar platilla single_post, no coge el css)

---
+++ src/Controller/SinglePostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SinglePostController extends AbstractController
+{
+    #[Route('/single/post', name: 'app_single_post')]
+    public function index(): Response
+    {
+        return $this->render('single_post.html.twig', [
+            'controller_name' => 'SinglePostController',
+        ]);
+    }
+}
