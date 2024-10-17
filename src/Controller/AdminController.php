9cfcd781bb5d665a2661f451078cf28a4b61d88e
bryanStv <bryans.oyola@gmail.com>
2024-10-17 10:40:38 +0200

    Añadido formulario de categorias

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Form\CategoryFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 
 class AdminController extends AbstractController
 {
@@ -12,5 +16,23 @@ class AdminController extends AbstractController
     public function images(): Response
     {
         return $this->render('admin/images.html.twig', []);
+    }
+
+    #[Route('/admin/categories', name: 'app_categories')]
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+            return $this->redirectToRoute('admin/categories', []);
+        }
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
