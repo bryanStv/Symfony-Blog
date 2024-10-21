509786b5c7f1b691c8716483f8d84d7c19da33c6
bryanStv <bryans.oyola@gmail.com>
2024-10-21 21:25:33 +0200

    Creada Entidad Post y Formulario, Además de la carga de imagenes y la validación de los campos 2

---
+++ src/Controller/PostController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class PostController extends AbstractController
+{
+    #[Route('/post', name: 'app_post')]
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+
+    #[Route('/blog/new', name: 'new_post')]
+    public function newPost(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setSlug($slugger->slug($post->getTitle()));
+            $post->setPostUser($this->getUser());
+            $post->setNumLikes(0);
+            $post->setNumComments(0);
+            $file = $form->get('file')->getData();
+
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $slugger->slug($originalFilename) . '-' . uniqid() . '.' . $file->guessExtension();
+
+                $file->move(
+                    $this->getParameter('images_directory'),
+                    $newFilename
+                );
+
+                $post->setImage($newFilename);
+            }
+
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+            return $this->render('blog/new_post.html.twig', array(
+                'form' => $form->createView()
+            ));
+        }
+        return $this->render('blog/new_post.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
