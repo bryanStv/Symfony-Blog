22504eac22ea2c6bca864cfd470625f1ba9281db
bryanStv <bryans.oyola@gmail.com>
2024-10-21 19:50:36 +0200

    Empezado carrusel imagenes

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_page')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('index.html.twig', [
-            'controller_name' => 'PageController',
-        ]);
+        $repository = $doctrine->getRepository(Category::class);
+
+        $categories = $repository->findAll();
+
+        return $this->render('index.html.twig', ['categories' => $categories]);
     }
 }
