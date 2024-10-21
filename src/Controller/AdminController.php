b39a10bf446d056a15e2db909cbe13b5bea40e30
bryanStv <bryans.oyola@gmail.com>
2024-10-21 08:33:11 +0200

    Formulario de agregar Imagenes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Image;
 use App\Form\CategoryFormType;
+use App\Form\ImageFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +14,6 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin/images', name: 'app_images')]
-    public function images(): Response
-    {
-        return $this->render('admin/images.html.twig', []);
-    }
 
     #[Route('/admin/categories', name: 'app_categories')]
     public function categories(ManagerRegistry $doctrine, Request $request): Response
@@ -39,5 +36,22 @@ class AdminController extends AbstractController
             'categories' => $categories
         ));
 
+    }
+
+    #[Route('/admin/images', name: 'app_images')]
+    public function images(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $image = new Image();
+        $form = $this->createForm(ImageFormType::class, $image);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $image = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($image);
+            $entityManager->flush();
+        }
+        return $this->render('admin/images.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
