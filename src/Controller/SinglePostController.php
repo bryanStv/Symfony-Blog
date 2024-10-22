ab1656486918d177eb99f6a9164c2e0f7c68621f
bryanStv <bryans.oyola@gmail.com>
2024-10-22 09:15:33 +0200

    Editado el Single Post

--- src/Controller/SinglePostController.php
+++ src/Controller/SinglePostController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SinglePostController extends AbstractController
 {
-    #[Route('/singlePost', name: 'app_single_post')]
-    public function index(): Response
+    #[Route('/singlePost/{slug}', name: 'app_single_post')]
+    public function post(ManagerRegistry $doctrine, $slug): Response
     {
+        $repositorio = $doctrine->getRepository(Post::class);
+        $post = $repositorio->findOneBy(["slug"=>$slug]);
         return $this->render('single_post.html.twig', [
-            'controller_name' => 'SinglePostController',
+            'post' => $post,
         ]);
     }
 }
