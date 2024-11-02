454b271affa57b10159ca801464795ee4b13078b
bryanStv <bryans.oyola@gmail.com>
2024-11-02 01:11:07 +0100

    Añadida la posibilidad de añadir comentarios

--- src/Controller/SinglePostController.php
+++ src/Controller/SinglePostController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\Comment;
+use App\Form\CommentFormType;
+use App\Repository\CommentRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -23,14 +27,33 @@ class SinglePostController extends AbstractController
         ]);
     }*/
     #[Route('/single_post/{slug}', name: 'app_single_post')]
-    public function post(PostRepository $postRepository, $slug): Response
+    public function post(PostRepository $postRepository,CommentRepository $commentRepository,ManagerRegistry $doctrine,Request $request,$slug): Response
     {
         $post = $postRepository->findOneBy(["slug" => $slug]);
+        
         $recents = $postRepository->findRecents();
+
+        $recentComments = $commentRepository->findBy(['post' => $post]);
+
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $comment->setPost($post);  
+            //Aumentamos en 1 el número de comentarios del post
+            $post->setNumComments($post->getNumComments() + 1);
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_single_post', ["slug" => $post->getSlug()]);
+        }
 
         return $this->render('blog/single_post.html.twig', [
             'post' => $post,
-            'recents' => $recents
+            'recents' => $recents,
+            'recentComments' => $recentComments,
+            'commentForm' => $form->createView()
         ]);
     }
 }
